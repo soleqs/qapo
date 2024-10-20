0180280e4e3372a039f78c7270e35b287f456b1b
my.b.ko <64437568+soleqs@users.noreply.github.com>
2024-10-20 23:06:36 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showTab(tabName) {
+  var tabs = document.getElementsByClassName('tab-content');
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = 'none';
+  }
+  document.getElementById(tabName).style.display = 'block';
+}
